5edb923a5ab1b17b62b0fdb5cbad16cf905a12df
Gabriel Lugo <g.alexander.lugo@gmail.com>
2021-09-07 05:18:11 -0400

    Update log.cpp

--- flugel/flugel_engine/src/flugel/log.cpp
+++ flugel/flugel_engine/src/flugel/log.cpp
@@ -25,28 +25,12 @@ namespace Flugel {
     }
   };
 
-  class file_formatter_flag : public spdlog::custom_flag_formatter {
-  public:
-    void format(const spdlog::details::log_msg& msg, const std::tm& tm, spdlog::memory_buf_t& dest) override {
-      std::string some_txt{msg.source.filename};
-      some_txt.push_back(':');
-      some_txt.append(std::to_string(msg.source.line));
-      dest.append(some_txt.data(), some_txt.data() + some_txt.size());
-    }
-
-    std::unique_ptr<custom_flag_formatter> clone() const override {
-      return spdlog::details::make_unique<file_formatter_flag>();
-    }
-  };
-
   std::shared_ptr<spdlog::logger> Log::engineLogger_;
   std::shared_ptr<spdlog::logger> Log::appLogger_;
 
   void Log::init() {
     auto formatter = std::make_unique<spdlog::pattern_formatter>();
-    formatter->add_flag<level_formatter_flag>('*')
-      .add_flag<file_formatter_flag>('N')
-      .set_pattern("%T %^%7*%$ %n: %v");
+    formatter->add_flag<level_formatter_flag>('*').set_pattern("%T %^%7*%$ %n: %v");
     spdlog::set_formatter(std::move(formatter));
     
     engineLogger_ = spdlog::stdout_color_mt("FLUGEL");
