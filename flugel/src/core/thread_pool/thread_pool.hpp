4eec6f9595423fcb5b78727a41cb1538b404d7e6
Gabriel Lugo <g.alexander.lugo@gmail.com>
2021-10-11 05:46:27 -0400

    Update thread_pool.hpp

--- flugel/src/core/thread_pool/thread_pool.hpp
+++ flugel/src/core/thread_pool/thread_pool.hpp
@@ -10,12 +10,6 @@ namespace fge {
 
     void pushJob(const JobFn& job);
   private:
-
-    /// TODO: THESE DEDICATED THREADS AREN'T NEEDED! JUST GIVE THE RENDER AND GAME JOBS
-    /// TO THE THREAD POOL! BECAUSE OF THE INFINITE LOOPS IN THOSE SPECIFIC JOBS, THE THREADS
-    /// WILL JUST KEEP GOING UNTIL THE APP STOPS!
-    //std::thread gameThread_;
-    //std::thread renderThread_;
     std::vector<std::thread> threadPool_{};
     std::queue<JobFn> jobQueue_{};
 
